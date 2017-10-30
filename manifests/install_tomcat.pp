c9681de04552d2b02fa8cc95f8411cc92b0e09d0
Bryan Belanger <bryan@autostructure.io>
2017-10-30 17:37:29 -0500

    Fix version # on install.

--- manifests/install_tomcat.pp
+++ manifests/install_tomcat.pp
@@ -1,8 +1,9 @@
 # Installs Java, a dependency for Tomcat
 class docker_tomcat_module::install_tomcat {
   tomcat_install { '/usr/local/tomcat':
-    ensure => present,
-    user   => $docker_tomcat_module::user,
-    group  => $docker_tomcat_module::group,
+    ensure  => present,
+    version => '7.0.82',
+    user    => $docker_tomcat_module::user,
+    group   => $docker_tomcat_module::group,
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,5 +11,7 @@ describe 'docker_tomcat_module' do
     }
 
     it { should contain_class('docker_tomcat_module') }
+
+    it { should contain_tomcat_install('/usr/local/tomcat') }
   end
 end

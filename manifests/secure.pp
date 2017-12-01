24e3b32b75413bb7c754cdfcf54c09ce64d6a3ef
Bryan Belanger <bryan@autostructure.io>
2017-12-01 13:43:56 -0500

    Remove hardening temporarily.

--- manifests/secure.pp
+++ manifests/secure.pp
@@ -1,21 +1,21 @@
 # Configure the Tomcat build
 class docker_tomcat_module::secure {
-  ::cis_harden_tomcat::harden_catalina_home { '/usr/local/tomcat':
-    user  => $docker_tomcat_module::user,
-    group => $docker_tomcat_module::group,
-  }
-
-  $docker_tomcat_module::wars.each | String $war, Hash $values | {
-    $war_simple_name = split($war, '[.]')
-
-    ::cis_harden_tomcat::harden_application { $war_simple_name[0]:
-      catalina_home => '/usr/local/tomcat',
-      catalina_base => $values['catalina_base'],
-      application   => $war_simple_name[0],
-      user          => $docker_tomcat_module::user,
-      group         => $docker_tomcat_module::group,
-    }
-  }
+  # ::cis_harden_tomcat::harden_catalina_home { '/usr/local/tomcat':
+  #   owner => $docker_tomcat_module::user,
+  #   group => $docker_tomcat_module::group,
+  # }
+# 
+  # $docker_tomcat_module::wars.each | String $war, Hash $values | {
+  #   $war_simple_name = split($war, '[.]')
+# 
+  #   ::cis_harden_tomcat::harden_application { $war_simple_name[0]:
+  #     catalina_home => '/usr/local/tomcat',
+  #     catalina_base => $values['catalina_base'],
+  #     application   => $war_simple_name[0],
+  #     user          => $docker_tomcat_module::user,
+  #     group         => $docker_tomcat_module::group,
+  #   }
+  # }
 
   # Remove unneeded application_source
   file { '/usr/local/tomcat/webapps/docs':
